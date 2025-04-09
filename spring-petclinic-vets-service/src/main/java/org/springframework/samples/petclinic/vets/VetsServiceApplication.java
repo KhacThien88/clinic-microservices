7099dc95f025455393e98c291192a32152acc25e
KhacThien88 <KTeightop1512@gmail.com>
2025-04-09 15:26:58 +0700

    test change

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -30,7 +30,7 @@ import org.springframework.samples.petclinic.vets.system.VetsProperties;
 public class VetsServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting VetsServiceApplicationt12345");
+		System.out.println("Starting VetsServiceApplicationt123456");
 		SpringApplication.run(VetsServiceApplication.class, args);
 	}
 }
