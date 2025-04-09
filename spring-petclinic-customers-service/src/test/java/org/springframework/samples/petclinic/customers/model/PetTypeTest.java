462c2b2281ac9844acbbf0ef5adca10c10347912
KhacThien88 <KTeightop1512@gmail.com>
2025-04-09 20:25:20 +0700

    update test case

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTypeTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTypeTest.java
@@ -15,55 +15,22 @@ class PetTypeTests {
         assertEquals(1, type.getId());
         assertEquals("Hamster", type.getName());
     }
-
     @Test
-    void testEqualsWithSameId() {
-        PetType type1 = new PetType();
-        type1.setId(1);
-        
-        PetType type2 = new PetType();
-        type2.setId(1);
-        
-        assertEquals(type1, type2, "PetTypes with same ID should be equal");
-    }
-
-    @Test
-    void testNotEqualsWithDifferentIds() {
-        PetType type1 = new PetType();
-        type1.setId(1);
-        
-        PetType type2 = new PetType();
-        type2.setId(2);
-        
-        assertNotEquals(type1, type2, "PetTypes with different IDs should not be equal");
-    }
-
-    @Test
-    void testHashCodeConsistency() {
+    void testPetTypeProperties1() {
         PetType type = new PetType();
-        type.setId(1);
-        type.setName("Hamster");
-        
-        int initialHashCode = type.hashCode();
-        assertEquals(initialHashCode, type.hashCode(), "Hash code should remain consistent");
-    }
+        type.setId(2);
+        type.setName("Hamsters");
 
-    @Test
-    void testToStringContainsName() {
-        PetType type = new PetType();
-        type.setName("Hamster");
-        
-        String toStringResult = type.toString();
-        assertTrue(toStringResult.contains("Hamster"), 
-            "toString() should include the pet type name");
+        assertEquals(2, type.getId());
+        assertEquals("Hamsters", type.getName());
     }
-
     @Test
-    void testEqualityWithDifferentClass() {
+    void testPetTypeProperties2() {
         PetType type = new PetType();
-        Object otherObject = new Object();
-        
-        assertNotEquals(type, otherObject, 
-            "PetType should not be equal to different class");
+        type.setId(3);
+        type.setName("Hamsterss");
+
+        assertEquals(3, type.getId());
+        assertEquals("Hamsters", type.getName());
     }
 }
