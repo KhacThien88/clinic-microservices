9cb1abff6d940aaad30b6cec352e7db79059a18c
KhacThien88 <KTeightop1512@gmail.com>
2025-04-09 20:21:32 +0700

    update test case

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTypeTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTypeTest.java
@@ -15,4 +15,55 @@ class PetTypeTests {
         assertEquals(1, type.getId());
         assertEquals("Hamster", type.getName());
     }
+
+    @Test
+    void testEqualsWithSameId() {
+        PetType type1 = new PetType();
+        type1.setId(1);
+        
+        PetType type2 = new PetType();
+        type2.setId(1);
+        
+        assertEquals(type1, type2, "PetTypes with same ID should be equal");
+    }
+
+    @Test
+    void testNotEqualsWithDifferentIds() {
+        PetType type1 = new PetType();
+        type1.setId(1);
+        
+        PetType type2 = new PetType();
+        type2.setId(2);
+        
+        assertNotEquals(type1, type2, "PetTypes with different IDs should not be equal");
+    }
+
+    @Test
+    void testHashCodeConsistency() {
+        PetType type = new PetType();
+        type.setId(1);
+        type.setName("Hamster");
+        
+        int initialHashCode = type.hashCode();
+        assertEquals(initialHashCode, type.hashCode(), "Hash code should remain consistent");
+    }
+
+    @Test
+    void testToStringContainsName() {
+        PetType type = new PetType();
+        type.setName("Hamster");
+        
+        String toStringResult = type.toString();
+        assertTrue(toStringResult.contains("Hamster"), 
+            "toString() should include the pet type name");
+    }
+
+    @Test
+    void testEqualityWithDifferentClass() {
+        PetType type = new PetType();
+        Object otherObject = new Object();
+        
+        assertNotEquals(type, otherObject, 
+            "PetType should not be equal to different class");
+    }
 }
