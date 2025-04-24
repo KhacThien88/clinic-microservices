3db45e11e184269fa631d0830d4a73f5d74999fb
KhacThien88 <KTeightop1512@gmail.com>
2025-04-24 11:44:06 +0700

    update change of vets service

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -30,7 +30,7 @@ import org.springframework.samples.petclinic.vets.system.VetsProperties;
 public class VetsServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting VetsServiceApplication65232224432232222");
+		System.out.println("Starting VetsServiceApplication652322244322232222");
 		SpringApplication.run(VetsServiceApplication.class, args);
 	}
 }
