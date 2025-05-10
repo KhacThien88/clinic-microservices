26a270c43c8c752176f7020515f59286d2724001
KhacThien88 <KTeightop1512@gmail.com>
2025-05-10 12:49:31 +0700

    change genai-service

--- spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/GenAIServiceApplication.java
+++ spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/GenAIServiceApplication.java
@@ -27,6 +27,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GenAIServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting VetsServiceApplicationt1234255627");
 		SpringApplication.run(GenAIServiceApplication.class, args);
 	}
 }
