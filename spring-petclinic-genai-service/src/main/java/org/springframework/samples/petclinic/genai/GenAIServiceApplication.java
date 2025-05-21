8bbfae38c775e469b702dcc608439805aa524072
KhacThien88 <KTeightop1512@gmail.com>
2025-05-22 01:43:02 +0700

    update

--- spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/GenAIServiceApplication.java
+++ spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/GenAIServiceApplication.java
@@ -27,7 +27,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GenAIServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting VetsServiceApplicationt1234252256227");
+		System.out.println("Starting VetsServiceApplicationt12342252256227");
 		SpringApplication.run(GenAIServiceApplication.class, args);
 	}
 }
