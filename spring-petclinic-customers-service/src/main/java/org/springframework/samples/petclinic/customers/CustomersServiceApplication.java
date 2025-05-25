ddbff5671cdd88cf4c6adbe9943cd4e1f70389e6
KhacThien88 <KTeightop1512@gmail.com>
2025-05-25 12:00:36 +0700

    update

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -27,7 +27,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class CustomersServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting VetsServiceAppliscsssss562222s222252222");
+		System.out.println("Starting VetsServiceAppliscsssss562222s2222252222");
 		SpringApplication.run(CustomersServiceApplication.class, args);
 	}
 }
