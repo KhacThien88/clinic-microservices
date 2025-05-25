dbd144a6bf6ff4e56517a3ffead28be5c98d5c33
KhacThien88 <KTeightop1512@gmail.com>
2025-05-25 12:01:25 +0700

    update

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
@@ -27,7 +27,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class VisitsServiceApplication {
 
     public static void main(String[] args) {
-        System.out.println("Starting VisitsServiceApplicationt123452622aaa225");
+        System.out.println("Starting VisitsServiceApplicationt1234526222aaa225");
         SpringApplication.run(VisitsServiceApplication.class, args);
     }
 }
